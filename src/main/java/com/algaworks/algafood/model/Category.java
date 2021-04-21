7dac7a2890773f088d7f3527dc878cc117e21c85
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-04-21 20:06:14 -0300

    Use Lombok @Data

--- src/main/java/com/algaworks/algafood/model/Category.java
+++ src/main/java/com/algaworks/algafood/model/Category.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
+@Data
 @Entity
 public class Category {
 
@@ -14,33 +16,4 @@ public class Category {
     private Long id;
 
     private String type;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Category)) return false;
-        Category category = (Category) o;
-        return getId().equals(category.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
 }
--- src/main/java/com/algaworks/algafood/model/Restaurant.java
+++ src/main/java/com/algaworks/algafood/model/Restaurant.java
@@ -1,12 +1,15 @@
 package com.algaworks.algafood.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.math.BigDecimal;
-import java.util.Objects;
 
+@Data
 @Entity
 public class Restaurant {
 
@@ -18,40 +21,6 @@ public class Restaurant {
 
     private BigDecimal shippingTax;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getShippingTax() {
-        return shippingTax;
-    }
-
-    public void setShippingTax(BigDecimal shippingTax) {
-        this.shippingTax = shippingTax;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Restaurant)) return false;
-        Restaurant that = (Restaurant) o;
-        return getId().equals(that.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId());
-    }
+    @ManyToOne
+    private Category category;
 }
