80c7326701dd2fda2d8c18a70e86e0784d3a4a98
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-04-21 20:11:43 -0300

    Add restaurant data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
-INSERT INTO algafood.category (category.type) VALUES ('Pizza');
-INSERT INTO algafood.category (category.type) VALUES ('Burger');
+-- POPULATE CATEGORY TABLE
+INSERT INTO algafood.category (category.id, category.type) VALUES (1, 'Australian');
+INSERT INTO algafood.category (category.id, category.type) VALUES (2, 'Burger');
+
+-- POPULATE RESTAURANT TABLE
+INSERT INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.shipping_tax, restaurant.category_id) VALUES (1, 'Outback', 15.00, 1);
+INSERT INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.shipping_tax, restaurant.category_id) VALUES (2, 'The Fifties', 8.00, 2);
