7c7638d85a2e6bb04476477ea14c52eeddfcd001
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-04-28 23:10:17 -0300

    Add media type to rest controllers

--- src/main/java/com/algaworks/algafood/controller/CategoryController.java
+++ src/main/java/com/algaworks/algafood/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.controller;
 import com.algaworks.algafood.model.Category;
 import com.algaworks.algafood.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("category")
+@RequestMapping(value = "category", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CategoryController {
 
     @Autowired
--- src/main/java/com/algaworks/algafood/controller/StateController.java
+++ src/main/java/com/algaworks/algafood/controller/StateController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.controller;
 import com.algaworks.algafood.model.State;
 import com.algaworks.algafood.repository.StateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("state")
+@RequestMapping(value = "state", produces = MediaType.APPLICATION_JSON_VALUE)
 public class StateController {
 
     @Autowired
