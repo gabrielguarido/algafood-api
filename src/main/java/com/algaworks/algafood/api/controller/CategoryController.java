2e0424b59d9da557d20eccba87bc1ebb39f4ab7d
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-05-08 19:36:47 -0300

    Implement POST endpoint to create categories

--- src/main/java/com/algaworks/algafood/api/controller/CategoryController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CategoryController.java
@@ -3,11 +3,15 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Category;
 import com.algaworks.algafood.domain.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -31,5 +35,11 @@ public class CategoryController {
         Category category = categoryRepository.findById(id).orElse(null);
 
         return ResponseEntity.ok(category);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category create(@RequestBody Category category) {
+        return categoryRepository.save(category);
     }
 }
