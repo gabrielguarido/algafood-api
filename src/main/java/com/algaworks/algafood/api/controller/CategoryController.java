fd6887ed21a202486393f13c0c7fc4ec2565bf67
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-05-08 19:51:44 -0300

    Implement PUT endpoint to update categories

--- src/main/java/com/algaworks/algafood/api/controller/CategoryController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Category;
 import com.algaworks.algafood.domain.repository.CategoryRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -34,12 +36,31 @@ public class CategoryController {
     public ResponseEntity<Category> findById(@PathVariable Long id) {
         Category category = categoryRepository.findById(id).orElse(null);
 
-        return ResponseEntity.ok(category);
+        if (category != null) {
+            return ResponseEntity.ok(category);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Category create(@RequestBody Category category) {
         return categoryRepository.save(category);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category category) {
+        Category existingCategory = categoryRepository.findById(id).orElse(null);
+
+        if (existingCategory != null) {
+            BeanUtils.copyProperties(category, existingCategory, "id");
+
+            categoryRepository.save(existingCategory);
+
+            return ResponseEntity.ok(existingCategory);
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
