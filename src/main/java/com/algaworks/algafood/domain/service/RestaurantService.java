501184f57e266077fe793d22eaed03481a6c8cb0
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-05-24 19:43:28 -0300

    Hot Fix

--- src/main/java/com/algaworks/algafood/domain/service/RestaurantService.java
+++ src/main/java/com/algaworks/algafood/domain/service/RestaurantService.java
@@ -75,7 +75,7 @@ public class RestaurantService {
     public Restaurant update(Long id, Restaurant restaurant) {
         var existingRestaurant = find(id);
 
-        BeanUtils.copyProperties(restaurant, existingRestaurant, "id", "paymentMethods", "address", "created", "updated");
+        BeanUtils.copyProperties(restaurant, existingRestaurant, "id", "paymentMethods", "address", "created");
 
         return save(existingRestaurant);
     }
