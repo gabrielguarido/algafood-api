fcf1a7cf661e96d060e544447e24c8bb4f3d8fdf
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-05-24 16:35:30 -0300

    Ignoring payment methods in the Restaurant update method

--- src/main/java/com/algaworks/algafood/domain/service/RestaurantService.java
+++ src/main/java/com/algaworks/algafood/domain/service/RestaurantService.java
@@ -75,7 +75,7 @@ public class RestaurantService {
     public Restaurant update(Long id, Restaurant restaurant) {
         var existingRestaurant = find(id);
 
-        BeanUtils.copyProperties(restaurant, existingRestaurant, "id");
+        BeanUtils.copyProperties(restaurant, existingRestaurant, "id", "paymentMethods");
 
         return save(existingRestaurant);
     }
