a0c52bcc2fca5cbf1c7696d895e740cf66226db1
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-06-12 22:44:05 -0300

    Create and map order and order_item tables

---
+++ src/main/java/com/algaworks/algafood/domain/model/OrderItem.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class OrderItem {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+    private Integer amount;
+    private String observation;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Product product;
+}
