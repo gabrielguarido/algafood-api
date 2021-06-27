f4553729e2424ebb96e38e0f1bc83fb76cd7084b
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-06-27 20:22:47 -0300

    Refactor Error.java structure

--- src/main/java/com/algaworks/algafood/api/exception/Error.java
+++ src/main/java/com/algaworks/algafood/api/exception/Error.java
@@ -1,14 +1,17 @@
 package com.algaworks.algafood.api.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @Getter
 @Builder
+@JsonInclude(NON_NULL)
 public class Error {
 
-    private final LocalDateTime timestamp;
-    private final String message;
+    private final Integer status;
+    private final String title;
+    private final String detail;
 }
