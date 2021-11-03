f72755c235cfeec7702ed348c95ee1f9d8a5f43d
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-11-03 19:58:48 -0300

    Implement Profile API

--- src/main/java/com/algaworks/algafood/domain/repository/ProfileRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/ProfileRepository.java
@@ -3,5 +3,8 @@ package com.algaworks.algafood.domain.repository;
 import com.algaworks.algafood.domain.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByName(String name);
 }
