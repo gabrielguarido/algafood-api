25285d5e553ae1c994b8ebdb89c1737a17d84eab
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2021-11-06 20:29:14 -0300

    Update method mapping

--- src/main/java/com/algaworks/algafood/api/controller/RestaurantPaymentMethodController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestaurantPaymentMethodController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +30,7 @@ public class RestaurantPaymentMethodController {
         return ResponseEntity.ok(restaurantService.listPaymentMethods(restaurantId));
     }
 
-    @PostMapping("/{paymentMethodId}")
+    @PutMapping("/{paymentMethodId}")
     public ResponseEntity<Void> add(@PathVariable Long restaurantId, @PathVariable Long paymentMethodId) {
         restaurantService.addPaymentMethod(restaurantId, paymentMethodId);
 
