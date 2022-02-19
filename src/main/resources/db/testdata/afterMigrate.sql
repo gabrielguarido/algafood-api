0c63c4e73e709a29319c6ab861fd8d42079539c7
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2022-02-18 21:59:07 -0300

    Add more test data

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -34,6 +34,7 @@ FROM restaurant_responsible_user;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
+
 -- RESET ID VALUES
 ALTER TABLE state
     AUTO_INCREMENT = 1;
@@ -66,23 +67,107 @@ ALTER TABLE user_profile
 ALTER TABLE restaurant_responsible_user
     AUTO_INCREMENT = 1;
 
+
 -- POPULATE STATE TABLE
 INSERT IGNORE INTO algafood.state (state.id, state.name)
 VALUES (1, 'São Paulo');
 INSERT IGNORE INTO algafood.state (state.id, state.name)
 VALUES (2, 'Rio de Janeiro');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (3, 'Acre');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (4, 'Alagoas');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (5, 'Amapá');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (6, 'Amazonas');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (7, 'Bahia');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (8, 'Ceará');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (9, 'Distrito Federal');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (10, 'Espirito Santo');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (11, 'Goiás');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (12, 'Maranhão');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (13, 'Mato Grosso');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (14, 'Mato Grosso do Sul');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (15, 'Minas Gerais');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (16, 'Pará');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (17, 'Paraíba');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (18, 'Paraná');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (19, 'Pernambuco');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (20, 'Piauí');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (21, 'Rio Grande do Norte');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (22, 'Rio Grande do Sul');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (23, 'Rondônia');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (24, 'Roraima');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (25, 'Santa Catarina');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (26, 'Sergipe');
+INSERT IGNORE INTO algafood.state (state.id, state.name)
+VALUES (27, 'Tocantins');
+
 
 -- POPULATE CITY TABLE
 INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
 VALUES (1, 'São Paulo', 1);
 INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
+VALUES (3, 'Guarulhos', 1);
+INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
 VALUES (2, 'Rio de Janeiro', 2);
+INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
+VALUES (4, 'Campinas', 1);
+INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
+VALUES (5, 'Ribeirão Preto', 1);
+INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
+VALUES (6, 'São Caetano', 1);
+INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
+VALUES (7, 'Santos', 1);
+INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
+VALUES (8, 'São Gonçalo', 2);
+INSERT IGNORE INTO algafood.city (city.id, city.name, city.state_id)
+VALUES (9, 'Niterói', 2);
+
 
 -- POPULATE CATEGORY TABLE
 INSERT IGNORE INTO algafood.category (category.id, category.type)
 VALUES (1, 'Australian');
 INSERT IGNORE INTO algafood.category (category.id, category.type)
 VALUES (2, 'Burger');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (3, 'Italian');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (4, 'Brazilian');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (5, 'French');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (6, 'Japanese');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (7, 'Chinese');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (8, 'Pizza');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (9, 'Sushi');
+INSERT IGNORE INTO algafood.category (category.id, category.type)
+VALUES (10, 'Steak');
+
 
 -- POPULATE RESTAURANT TABLE
 INSERT IGNORE INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.delivery_fee, restaurant.category_id,
@@ -94,12 +179,30 @@ VALUES (1, 'Outback', 15.00, 1, '03112-090', 'Rua Marina Crespi', '274', 'Aparta
 INSERT IGNORE INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.delivery_fee, restaurant.category_id,
                                         restaurant.created, restaurant.updated)
 VALUES (2, 'The Fifties', 8.00, 2, utc_timestamp, utc_timestamp);
+INSERT IGNORE INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.delivery_fee, restaurant.category_id,
+                                        restaurant.created, restaurant.updated)
+VALUES (3, 'Fogo de Chão', 25.00, 4, utc_timestamp, utc_timestamp);
+INSERT IGNORE INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.delivery_fee, restaurant.category_id,
+                                        restaurant.created, restaurant.updated)
+VALUES (4, 'Sushi One', 5.00, 9, utc_timestamp, utc_timestamp);
+INSERT IGNORE INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.delivery_fee, restaurant.category_id,
+                                        restaurant.created, restaurant.updated)
+VALUES (5, 'China in Box', 0.00, 7, utc_timestamp, utc_timestamp);
+INSERT IGNORE INTO algafood.restaurant (restaurant.id, restaurant.name, restaurant.delivery_fee, restaurant.category_id,
+                                        restaurant.created, restaurant.updated)
+VALUES (6, 'McDonald\'s', 0.00, 7, utc_timestamp, utc_timestamp);
+
 
 -- POPULATE PAYMENT_METHOD TABLE
 INSERT IGNORE INTO algafood.payment_method (payment_method.id, payment_method.method)
 VALUES (1, 'CREDIT CARD');
 INSERT IGNORE INTO algafood.payment_method (payment_method.id, payment_method.method)
 VALUES (2, 'CASH');
+INSERT IGNORE INTO algafood.payment_method (payment_method.id, payment_method.method)
+VALUES (3, 'DEBIT CARD');
+INSERT IGNORE INTO algafood.payment_method (payment_method.id, payment_method.method)
+VALUES (4, 'FOOD TICKET');
+
 
 -- POPULATE PERMISSION TABLE
 INSERT IGNORE INTO algafood.permission (permission.id, permission.role)
@@ -107,30 +210,57 @@ VALUES (1, 'CREATE');
 INSERT IGNORE INTO algafood.permission (permission.id, permission.role)
 VALUES (2, 'UPDATE');
 
+
 -- POPULATE RESTAURANT_PAYMENT_METHOD TABLE
 INSERT IGNORE INTO algafood.restaurant_payment_method (restaurant_payment_method.restaurant_id,
                                                        restaurant_payment_method.payment_method_id)
 VALUES (1, 1),
        (1, 2),
+       (1, 3),
+       (1, 4),
        (2, 1),
-       (2, 2);
+       (2, 2),
+       (2, 3),
+       (2, 4),
+       (3, 1),
+       (3, 2),
+       (3, 3),
+       (3, 4),
+       (4, 1),
+       (4, 2),
+       (4, 3),
+       (4, 4),
+       (5, 1),
+       (5, 2),
+       (5, 3),
+       (5, 4),
+       (6, 1),
+       (6, 2),
+       (6, 3),
+       (6, 4);
+
 
 -- POPULATE PRODUCT TABLE
 INSERT IGNORE INTO algafood.product (product.id, product.active, product.description, product.name, product.price,
                                      product.restaurant_id)
-VALUES (1, 1, 'Delicious bacon double burger', 'Double Bacon', 33.50, 2);
+VALUES (1, 1, 'Bacon double burger', 'Double Bacon', 33.50, 2);
 INSERT IGNORE INTO algafood.product (product.id, product.active, product.description, product.name, product.price,
                                      product.restaurant_id)
 VALUES (2, 1, 'Cheddar bacon burger', 'Ned Kelly', 40.00, 1);
 INSERT IGNORE INTO algafood.product (product.id, product.active, product.description, product.name, product.price,
                                      product.restaurant_id)
 VALUES (3, 1, 'Coke Can', 'Coca-Cola', 8.90, 1);
+INSERT IGNORE INTO algafood.product (product.id, product.active, product.description, product.name, product.price,
+                                     product.restaurant_id)
+VALUES (4, 1, 'Coke Can', 'Coca-Cola', 5.50, 2);
+
 
 -- POPULATE PROFILE TABLE
 INSERT IGNORE INTO algafood.profile (profile.id, profile.name)
 VALUES (1, 'ADMIN');
 INSERT IGNORE INTO algafood.profile (profile.id, profile.name)
 VALUES (2, 'QA');
+
 
 -- POPULATE PROFILE_PERMISSION TABLE
 INSERT IGNORE INTO algafood.profile_permission (profile_permission.profile_id, profile_permission.permission_id)
@@ -139,11 +269,13 @@ VALUES (1, 1),
        (2, 1),
        (2, 2);
 
+
 -- POPULATE USER TABLE
 INSERT IGNORE INTO algafood.user (user.id, user.created, user.email, user.name, user.password)
-VALUES (1, utc_timestamp, 'gabriel@gmail.com', 'Gabriel Oliveira', '123');
+VALUES (1, utc_timestamp, 'gabrielguarido.oliveira@gmail.com', 'Gabriel Oliveira', '123');
 INSERT IGNORE INTO algafood.user (user.id, user.created, user.email, user.name, user.password)
 VALUES (2, utc_timestamp, 'qa@gmail.com', 'QA Team', '123');
+
 
 -- POPULATE USER_PROFILE TABLE
 INSERT IGNORE INTO algafood.user_profile (user_profile.user_id, user_profile.profile_id)
@@ -152,11 +284,13 @@ VALUES (1, 1),
        (2, 1),
        (2, 2);
 
+
 -- POPULATE RESTAURANT_RESPONSIBLE_USER TABLE
 INSERT IGNORE INTO algafood.restaurant_responsible_user (restaurant_responsible_user.restaurant_id,
                                                          restaurant_responsible_user.user_id)
 VALUES (1, 1),
        (2, 2);
+
 
 -- POPULATE ORDER TABLE
 INSERT IGNORE INTO algafood.`order` (`order`.id, `order`.external_key, `order`.subtotal, `order`.delivery_fee,
@@ -179,6 +313,7 @@ INSERT IGNORE INTO algafood.`order` (`order`.id, `order`.external_key, `order`.s
                                      `order`.cancelled, `order`.delivered)
 VALUES (2, '450b0c9a-4a45-11ec-85ee-fc34977b31ad', 33.50, 0.00, 33.50, 2, 2, 2, 1, '03112-090', 'Rua Marina Crespi',
         '274', 'Apartamento 12 A', 'Mooca - SP', 'DELIVERED', utc_timestamp, utc_timestamp, null, utc_timestamp);
+
 
 -- POPULATE ORDER_ITEM TABLE
 INSERT IGNORE INTO algafood.order_item (order_item.id, order_item.amount, order_item.unit_price, order_item.total_price,
