f5188337c2a10d565a787429c0329cb94078a625
Gabriel Oliveira <gabrielguarido.oliveira@gmail.com>
2022-03-01 14:46:33 -0300

    Log infos

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalPictureStorageServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/LocalPictureStorageServiceImpl.java
@@ -4,18 +4,20 @@ import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.storage.PictureStorageService;
 import com.algaworks.algafood.infrastructure.exception.StorageException;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.FileCopyUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+@Slf4j
 @NoArgsConstructor
 public class LocalPictureStorageServiceImpl implements PictureStorageService {
 
     private static final String STORE_EXCEPTION_MESSAGE = "It was not possible to store the file %s in the local storage";
-    private static final String REMOVE_EXCEPTION_MESSAGE = "It was not possible to remove the file %s  from local storage";
-    private static final String RETRIEVE_EXCEPTION_MESSAGE = "It was not possible to retrieve the file %s  from local storage";
+    private static final String REMOVE_EXCEPTION_MESSAGE = "It was not possible to remove the file %s  from the local storage";
+    private static final String RETRIEVE_EXCEPTION_MESSAGE = "It was not possible to retrieve the file %s from the local storage";
 
     @Autowired
     private StorageProperties storageProperties;
@@ -24,6 +26,8 @@ public class LocalPictureStorageServiceImpl implements PictureStorageService {
     public void store(Picture picture) {
         try {
             var filePath = Path.of(storageProperties.getLocal().getPath(), picture.getFileName());
+
+            log.info("Storing the file {} in the local storage", filePath);
 
             FileCopyUtils.copy(picture.getInputStream(), Files.newOutputStream(filePath));
         } catch (Exception e) {
@@ -36,6 +40,8 @@ public class LocalPictureStorageServiceImpl implements PictureStorageService {
         try {
             var filePath = Path.of(storageProperties.getLocal().getPath(), fileName);
 
+            log.info("Removing the file {} from the local storage", filePath);
+
             Files.deleteIfExists(filePath);
         } catch (Exception e) {
             throw new StorageException(String.format(REMOVE_EXCEPTION_MESSAGE, fileName), e);
@@ -46,6 +52,8 @@ public class LocalPictureStorageServiceImpl implements PictureStorageService {
     public PictureResponse retrieve(String fileName) {
         try {
             var filePath = Path.of(storageProperties.getLocal().getPath(), fileName);
+
+            log.info("Retrieving the file {} from the local storage", filePath);
 
             return PictureResponse.builder()
                     .inputStream(Files.newInputStream(filePath))
